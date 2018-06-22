c27428fc5f8a077052919275c1463c4bd841da4c
Lung Chen <lungheichen@gmail.com>
2018-06-22 22:40:41 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(arr) {
   for (var i = 0; i < 25; i++) {
     if (i=1) {
-      arr =+ [`I am 1 strange loop.`];
+      arr.push(`I am 1 strange loop.`);
     }
     else {
-      arr =+ [`I am ${i} strange loops.`];
+      arr.push(`I am ${i} strange loops.`);
     }
   }
 }
